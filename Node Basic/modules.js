a955f6b4b81958d5c571b6e62cee6f7ee6c0d771
Mohammed Hassan <mh1921544@gmail.com>
2023-10-18 11:29:58 +0530

    Changes to be committed: 	new file:   Node Basic/http.js 	modified:   Node Basic/modules.js

--- Node Basic/modules.js
+++ Node Basic/modules.js
@@ -12,9 +12,19 @@ var tobj = setTimeout(timer_eg, 2000); */
 
 // file system module
 
-var fs = require("fs");
+/* var fs = require("fs");
 var data = fs.readFileSync('C:\Users\mh192\OneDrive\Documents\Semester 3\Web-Technology\Node Basic\test.txt');
 console.log(data.toString());
 console.log("Ended");
+ */
 
-
+var fs = require("fs");
+var data = fs.readFile('C:\Users\mh192\OneDrive\Documents\Semester 3\Web-Technology\Node Basic\test.txt', function (err, data) {
+    if (err)
+        return console.log("Error");
+    else
+        console.log(data.toString());
+    setTimeout(() => { 
+        console.log("Ended");
+    }, 2000);
+    
