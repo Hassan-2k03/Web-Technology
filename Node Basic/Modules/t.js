cd8edc936ca7b8317b5879fbaa0520411d9449f6
Mohammed Hassan <mh1921544@gmail.com>
2023-11-29 19:23:23 +0530

    Deleted unnecessary files and modules

--- Node Basic/Modules/t.js
+++
@@ -1,8 +0,0 @@
-var chk = require('chalk');
-console.log(chk.blue('Hello world!'));
-console.log(chk.red('Hello world!'));
-console.log(chk.green('Hello world!'));
-console.log(chk.yellow('Hello world!'));
-console.log(chk.magenta('Hello world!'));
-console.log(chk.cyan('Hello world!'));
-console.log(chk.white('Hello world!'));
--- Node Basic/Modules/validator.mjs
+++
@@ -1,6 +0,0 @@
-const {isEmail,isLowercase} = pkg;
-import pkg from 'validator';
-
-var email = 'abc@gmail.com';
-console.log(isEmail(email));
-console.log(isLowercase(email));
